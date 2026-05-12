f329816a4ab4b4c2b5fcdf1e3e0436e7e182343a
Siya <siya@example.com>
2026-05-12 18:28:29 +0530

    event export update

--- app/Exports/EventTransactionExport.php
+++ app/Exports/EventTransactionExport.php
@@ -4,146 +4,138 @@ namespace App\Exports;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use App\Models\EventTransaction;
+use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-
-class EventTransactionExport implements FromCollection, WithHeadings
+class EventTransactionExport implements FromQuery, WithHeadings
 {
-
-    protected $request;
+    protected Request $request;
 
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
-
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    public function query()
     {
         $request = $this->request;
-        //dd( $request);
+
         $query = DB::connection('mysql2')
             ->table('event_transactions')
             ->leftJoin('learners', 'event_transactions.learner_id', '=', 'learners.id')
             ->leftJoin('yuwaah_event_masters', 'event_transactions.event_category', '=', 'yuwaah_event_masters.id')
             ->leftJoin('yuwaah_event_type', 'yuwaah_event_masters.event_type_id', '=', 'yuwaah_event_type.id')
             ->leftJoin('yuwaah_sakhi', 'event_transactions.ys_id', '=', 'yuwaah_sakhi.id')
-            ->leftJoin(DB::raw('
+            ->leftJoin(DB::raw("
                 (
-                    SELECT *
+                    SELECT c1.*
                     FROM yuwaah_backend.event_transaction_comments c1
-                    WHERE c1.id = (
-                        SELECT MAX(c2.id)
-                        FROM yuwaah_backend.event_transaction_comments c2
-                        WHERE c2.event_transaction_id = c1.event_transaction_id
-                    )
+                    INNER JOIN (
+                        SELECT event_transaction_id, MAX(id) as max_id
+                        FROM yuwaah_backend.event_transaction_comments
+                        GROUP BY event_transaction_id
+                    ) c2 ON c1.id = c2.max_id
                 ) as latest_comments
-            '), 'latest_comments.event_transaction_id', '=', 'event_transactions.id')
-    
-            ->select(
-                'event_transactions.id',  
-                'event_transactions.review_status',  
-                'yuwaah_event_type.name as event_name', 
-                'yuwaah_event_masters.event_category',   
-                'event_transactions.beneficiary_name',       
-                'event_transactions.beneficiary_phone_number',     
-                'learners.PROGRAM_CODE',
-                'learners.PROGRAM_STATE',
-                'learners.PROGRAM_DISTRICT',
-                'yuwaah_sakhi.name',      
-                'yuwaah_sakhi.sakhi_id',    
-                'yuwaah_event_masters.document_1 as document_1',
-                'yuwaah_event_masters.document_2 as document_2',
-                'yuwaah_event_masters.document_3 as document_3',  
-                'event_transactions.created_at',     
-                'event_transactions.event_date_submitted',    // Event Created
-                'event_transactions.event_value',                       // Event Value
-                'event_transactions.field_type',                        // Field Type
-                'event_transactions.comment',                           // Comment
-                'event_transactions.updated_at',   
-                'latest_comments.status as latest_status',
-                'latest_comments.comment as latest_comment',
-                'latest_comments.comment_type as latest_comment_type',
-            )
-            ->where('yuwaah_sakhi.csc_id','!=','Sandbox_Testing')
+            "), 'latest_comments.event_transaction_id', '=', 'event_transactions.id')
+            ->where('yuwaah_sakhi.csc_id', '!=', 'Sandbox_Testing')
             ->whereNotNull('event_transactions.review_status')
             ->whereNotNull('event_transactions.event_date_submitted')
             ->whereNotNull('event_transactions.learner_id')
+            ->select(
+                'event_transactions.id',
+                'event_transactions.review_status',
+                'yuwaah_event_type.name as event_name',
+                'yuwaah_event_masters.event_category',
+                'event_transactions.beneficiary_name',
+                'event_transactions.beneficiary_phone_number',
+                'learners.PROGRAM_CODE',
+                'learners.PROGRAM_STATE',
+                'learners.PROGRAM_DISTRICT',
+                'yuwaah_sakhi.name as agent_name',
+                'yuwaah_sakhi.sakhi_id',
+                'yuwaah_event_masters.document_1',
+                'yuwaah_event_masters.document_2',
+                'yuwaah_event_masters.document_3',
+                'event_transactions.created_at',
+                'event_transactions.event_date_submitted',
+                'event_transactions.event_value',
+                'event_transactions.field_type',
+                'event_transactions.comment',
+                'event_transactions.updated_at',
+                'latest_comments.status as latest_status',
+                'latest_comments.comment as latest_comment',
+                'latest_comments.comment_type as latest_comment_type'
+            )
             ->orderBy('event_transactions.id', 'desc');
-        //$query->where('event_transactions.review_status', $request->status);
-        /** APPLY FILTERS (CORRECT WAY) */
+
         if ($request->filled('submit')) {
-    
-            // STATUS
+
             if ($request->filled('status')) {
                 $query->where('event_transactions.review_status', $request->status);
             }
-    
-            // EVENT TYPE
-            if ($request->event_type > 0) {
+
+            if ($request->filled('event_type') && (int) $request->event_type > 0) {
                 $query->where('yuwaah_event_type.id', $request->event_type);
             }
-    
-            // EVENT CATEGORY
-            if ($request->event_category > 0) {
+
+            if ($request->filled('event_category') && (int) $request->event_category > 0) {
                 $query->where('event_transactions.event_category', $request->event_category);
             }
-    
-            // DATE RANGE
-            if ($request->from_date && $request->to_date) {
+
+            if ($request->filled('from_date') && $request->filled('to_date')) {
                 $query->whereBetween('event_transactions.event_date_submitted', [
-                    $request->from_date,
-                    $request->to_date
+                    $request->from_date . ' 00:00:00',
+                    $request->to_date . ' 23:59:59',
                 ]);
             }
-            
-    
-            // BENEFICIARY NAME
+
             if ($request->filled('submitted_date')) {
-                $query->where('event_transactions.event_date_submitted', $request->submitted_date);
+                $query->whereDate('event_transactions.event_date_submitted', $request->submitted_date);
             }
 
-             // BENEFICIARY NAME
-             if ($request->filled('benificiery_name')) {
-                $query->where('event_transactions.beneficiary_name', 'like', "%{$request->benificiery_name}%");
+            if ($request->filled('benificiery_name')) {
+                $query->where(
+                    'event_transactions.beneficiary_name',
+                    'like',
+                    '%' . $request->benificiery_name . '%'
+                );
             }
-    
-            // BENEFICIARY MOBILE
+
             if ($request->filled('benificiery_mobile')) {
-                $query->where('event_transactions.beneficiary_phone_number', 'like', "%{$request->benificiery_mobile}%");
+                $query->where(
+                    'event_transactions.beneficiary_phone_number',
+                    'like',
+                    '%' . $request->benificiery_mobile . '%'
+                );
             }
-    
 
-            // BENEFICIARY MOBILE
             if ($request->filled('program_code')) {
                 $query->where('learners.PROGRAM_CODE', $request->program_code);
             }
 
             if ($request->filled('sakhi_id')) {
-                $query->where('yuwaah_sakhi.sakhi_id', 'like', "%{$request->sakhi_id}%");
+                $query->where(
+                    'yuwaah_sakhi.sakhi_id',
+                    'like',
+                    '%' . $request->sakhi_id . '%'
+                );
             }
 
-            
-    
-            // GLOBAL SEARCH
             if ($request->filled('search_text')) {
                 $search = $request->search_text;
+
                 $query->where(function ($q) use ($search) {
-                    $q->where('event_transactions.beneficiary_name', 'like', "%$search%")
-                      ->where('event_transactions.beneficiary_phone_number', 'like', "%$search%")
-                      ->where('event_transactions.event_value', 'like', "%$search%");
+                    $q->where('event_transactions.beneficiary_name', 'like', '%' . $search . '%')
+                        ->orWhere('event_transactions.beneficiary_phone_number', 'like', '%' . $search . '%')
+                        ->orWhere('event_transactions.event_value', 'like', '%' . $search . '%')
+                        ->orWhere('yuwaah_sakhi.sakhi_id', 'like', '%' . $search . '%')
+                        ->orWhere('learners.PROGRAM_CODE', 'like', '%' . $search . '%');
                 });
             }
         }
-    
-        return $query->get();
+
+        return $query;
     }
-   
 
     public function headings(): array
     {
@@ -155,8 +147,8 @@ class EventTransactionExport implements FromCollection, WithHeadings
             'Beneficiary Name',
             'Number',
             'Program Code',
-            'Pograme State',
-            'Pograme City',
+            'Program State',
+            'Program City',
             'Agent Name',
             'Agent ID',
             'Required Document 1',
@@ -170,7 +162,7 @@ class EventTransactionExport implements FromCollection, WithHeadings
             'Last Updated',
             'Latest Status',
             'Latest Comment',
-            'Latest Comment Type'
+            'Latest Comment Type',
         ];
     }
 }
